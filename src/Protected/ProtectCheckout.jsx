f238406fb4a5f4b839fcf1d4011d333e5579ede7
Abiram-k <abiramk0107@gmail.com>
2024-11-28 19:16:19 +0530

    protecting routes

--- src/Protected/ProtectCheckout.jsx
+++ src/Protected/ProtectCheckout.jsx
@@ -3,10 +3,14 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectCheckout = ({ children }) => {
+
   const checkoutItems = useSelector((state) => state.users.checkoutItems);
-  console.log("CHECKOUT ITEMS", checkoutItems);
-  alert(checkoutItems)
-  if (checkoutItems.length == 0) return <Navigate to="/user/cart" />;
-  else return children;
-};
-export default ProtectCheckout;
+
+  if (!checkoutItems)
+     return <Navigate to="/user/cart" />;
+  else
+     return children;
+
+}
+
+export default ProtectCheckout
