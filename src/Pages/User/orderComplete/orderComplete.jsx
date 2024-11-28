897a2f7ffdf1d302602e7bda0d55d1e3312fbe2a
Abiram-k <abiramk0107@gmail.com>
2024-11-28 19:38:02 +0530

    fixing bugs

--- src/Pages/User/orderComplete/orderComplete.jsx
+++ src/Pages/User/orderComplete/orderComplete.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const orderComplete = () => {
+  const navigate = useNavigate();
+  const handleRedirect = () => {
+    navigate("/user/shop");
+  };
   return (
-    <main className="flex-grow flex flex-col items-center justify-center text-center p-4"style={{  fontfamily: "Montserrat"
-    }}>
+    <main
+      className="flex-grow flex flex-col items-center justify-center text-center p-4"
+      style={{ fontfamily: "Montserrat" }}
+    >
       <div className="bg-gray-800 rounded-full px-6 py-2 mb-4">
         <span className="text-xl font-bold">Order Completed</span>
         <span className="block text-green-500">Arriving By Wed, Apr 2024</span>
@@ -13,7 +19,7 @@ const orderComplete = () => {
         to="user/profile/myOrders"
         className="bg-gray-200 text-black rounded-full px-6 py-2 mb-8"
       >
-        View order 
+        View order
       </Link>
       <div className="text-6xl text-green-500 mb-4">
         <i className="fas fa-check-circle"></i>
@@ -23,12 +29,13 @@ const orderComplete = () => {
         Thank You for your order, Sit tight we are processing your order we will
         update you with your order in email
       </p>
-      <Link
-        to={"/user/shop"}
+
+      <button
+        onClick={() => handleRedirect()}
         className="bg-red-600 text-white rounded-full px-6 py-2"
       >
         Continue Shopping
-      </Link>
+      </button>
     </main>
   );
 };
