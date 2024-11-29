52454707e3d6a32419af5d06fb19b541c35856b1
Abiram-k <abiramk0107@gmail.com>
2024-11-30 00:03:30 +0530

    fixed bug

--- src/Protected/ProtectOtp.jsx
+++ src/Protected/ProtectOtp.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedOtp = ({ children }) => {
   const signUpSuccess = useSelector((state) => state.users.signUpSuccess);
-  if (!signUpSuccess) return <Navigate to={"/signup"} />;
+  if (!signUpSuccess) return <Navigate to={"/login"} />;
   return children;
 };
 
