a9f449d22590c38712f224204557584f0d2816b7
Abiram-k <abiramk0107@gmail.com>
2025-06-23 00:29:25 +0530

    fix google auth

--- src/Pages/User/login/Login.jsx
+++ src/Pages/User/login/Login.jsx
@@ -72,7 +72,7 @@ function Login() {
   };
   const handleGoogleAuth = () => {
     window.location.href = `https://skillpulsebackend.abiram.website/auth/google?method=login`;
-    // window.location.href = `${BACKEND_URL}/auth/google?method=login`;
+    // window.location.href = `http://localhost:3000/auth/google?method=login`;
   };
 
   const toggleReferral = () => {
